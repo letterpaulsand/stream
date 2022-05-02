a9016ce088507d2c22427d212566e2fd3a4344fe
paul097958 <paul097958@gmail.com>
2022-05-03 00:46:44 +0800

    not

---
+++ src/class/index.js
@@ -0,0 +1,148 @@
+import "../global/global.css"
+import "./index.css"
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../global/firebase.js"
+const video = document.getElementById('video')
+const container = document.getElementById('container')
+const come = document.getElementById('come')
+const leave = document.getElementById('leave')
+var peer = new Peer({
+    debug: 3,
+    config: {
+        iceServers: [{
+            urls: 'stun:stun.l.google.com:19302',
+        }],
+        iceTransportPolicy: 'all'
+    },
+    secure: true
+});
+const url = new URL(location.href);
+const v = url.searchParams.get('v');
+
+function checkTheRoom() {
+    if (!v) {
+        alert('You don\'t have a room number')
+        location.href = './start'
+    }
+}
+
+
+async function startGetPeer(stream) {
+    const docRef = doc(db, "room", v);
+    const docSnap = await getDoc(docRef);
+    let data = docSnap.data().user
+    console.log(data);
+    data.map(item => {
+        callStream(item, stream)
+    })
+}
+
+async function changeTheCurrentPeopleList(arrOfCurrentPeople){
+    if(!id) return
+    console.log(arrOfCurrentPeople);
+    await setDoc(doc(db, 'room', v), {
+        user: [
+            ...arrOfCurrentPeople,
+            id
+        ]
+    });
+}
+
+
+async function addMeToDatabase(id) {
+    try {
+        await setDoc(doc(db, 'room', v), {
+            user: [
+                id
+            ]
+        });
+    } catch (e) {
+        alert('The room id is incorrect!')
+        location.href = './start'
+    }
+}
+
+peer.on('open', async function (id) {
+    // When the peer open start searching the member in the database
+    startGetPeer(video.srcObject)
+    addMeToDatabase(id)
+});
+
+function getCallEvent(stream) {
+    peer.on('call', data => {
+        data.answer(stream)
+        data.on('stream', remoteStream => {
+            come.play()
+            let videoNew = document.createElement('video')
+            newVideo(data.peer, videoNew, remoteStream)
+            checkOnline(videoNew)
+        })
+    })
+}
+
+async function getCameraAndSendStream() {
+    let constructor = {
+        video: true,
+        audio: true
+    }
+    try {
+        let stream = await navigator.mediaDevices.getUserMedia(constructor)
+        video.srcObject = stream
+        getCallEvent(stream)
+    } catch (e) {
+        console.error(e);
+    }
+
+}
+
+function checkOnline(video) {
+    // if the video tag doesn't play for 3 second it will be disappear
+    let videoCurrent = video.currentTime;
+    let leaveState = false
+    setInterval(async () => {
+        if (leaveState) {
+            return
+        }
+        let videoCheck = video.currentTime
+        if (videoCurrent == videoCheck) {
+            video.style.display = 'none'
+            leaveState = true
+            leave.play()
+        } else {
+            videoCurrent = video.currentTime
+        }
+    }, 3000)
+}
+
+
+function callStream(remoteId, mediaStream) {
+    console.log('I call ' + remoteId);
+    let callStream = peer.call(remoteId, mediaStream)
+    callStream.on('stream', remoteStream => {
+        come.play()
+        let videoNew = document.createElement('video')
+        addListArr.unshift(remoteId)
+        console.log(addListArr);
+        changeTheCurrentPeopleList(addListArr)
+        newVideo(remoteId, videoNew, remoteStream)
+        checkOnline(videoNew)
+    })
+}
+
+function newVideo(remoteId, video, remoteStream) {
+    let checkVideoElement = document.querySelector(`[data-code='${remoteId}']`)
+    if (checkVideoElement) return
+    video.dataset.code = remoteId
+    video.classList.add('video')
+    video.classList.add('border')
+    video.classList.add('border-white')
+    video.classList.add('border-4')
+    video.srcObject = remoteStream
+    video.dataset.code = remoteId
+    video.autoplay = true
+    container.appendChild(video)
+}
+
+
+getCameraAndSendStream()
+checkTheRoom()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
-    entry: './public/index.js',
+    entry: './src/class/index.js',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    devtool: 'source-map'
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
+          {
+            test: /\.wav/,
+            type: 'asset/resource'
+          }
+        ],
+      },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve('./src/class/index.html'),
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    devServer: {
+        hot: true,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        }
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    }
 };
